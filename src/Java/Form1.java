f59369c45082aa3fa09dae09cc382bb9f2590f13
Mateor13 <mateo.torres@epn.edu.ec>
2024-06-20 09:49:23 -0500

    JOptionPane

--- src/Java/Form1.java
+++ src/Java/Form1.java
@@ -1,4 +1,10 @@
 package Java;
 
+import javax.swing.*;
+
 public class Form1 {
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        String primerNumero = JOptionPane.showInputDialog("Introduzca el primer numero");
+        String segundoNumero = JOptionPane.showInputDialog("Introduzca el segundo numero");
+
+        int numero1 = Integer.parseInt(primerNumero);
+        int numero2 = Integer.parseInt(segundoNumero);
+        int suma = numero1 + numero2;
+
+        JOptionPane.showMessageDialog(null, "El resultado de la suma es: " + suma, "Suma de dos enteros", JOptionPane.PLAIN_MESSAGE);
     }
 }
