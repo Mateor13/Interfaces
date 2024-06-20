eb0b3d679844ddacd6656aa224b7f67634a4fc98
Mateor13 <mateo.torres@epn.edu.ec>
2024-06-20 09:57:01 -0500

    JOptionPane

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,11 @@ public class Main {
         int numero1 = Integer.parseInt(primerNumero);
         int numero2 = Integer.parseInt(segundoNumero);
         int suma = numero1 + numero2;
+        int resto = numero1 - numero2;
+        int multiplicacion = numero1 * numero2;
+        int division = numero1 / numero2;
 
-        JOptionPane.showMessageDialog(null, "El resultado de la suma es: " + suma, "Suma de dos enteros", JOptionPane.PLAIN_MESSAGE);
+
+        JOptionPane.showMessageDialog(null, "El resultado de la suma es: " + suma+"\nLa resta es: "+ resto +"\nLa multiplicación es "+multiplicacion +"\nLa división es: "+ division, "Suma de dos enteros", JOptionPane.PLAIN_MESSAGE);
     }
 }
