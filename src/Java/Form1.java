423afb67208723d9ec75e064de90e27f97059232
Mateor13 <mateo.torres@epn.edu.ec>
2024-06-21 11:44:11 -0500

    Personalizar ventana

--- src/Java/Form1.java
+++ src/Java/Form1.java
@@ -1,10 +1,29 @@
 package Java;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Form1 {
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
+    private JButton btnOK;
+    public JPanel mainPanel;
+    private javax.swing.JLabel JLabel;
+    private JButton borrarButton;
+
+    public Form1() {
+        btnOK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JLabel.setText("Hola a todos, yo soy Mateo Torres");
+            }
+        });
+        borrarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JLabel.setText("");
+            }
+        });
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import javax.swing.*;
+import Java.*;
 
 public class Main {
     public static void main(String[] args) {
-        String primerNumero = JOptionPane.showInputDialog("Introduzca el primer numero");
+        /*String primerNumero = JOptionPane.showInputDialog("Introduzca el primer numero");
         String segundoNumero = JOptionPane.showInputDialog("Introduzca el segundo numero");
 
         int numero1 = Integer.parseInt(primerNumero);
@@ -10,9 +11,16 @@ public class Main {
         int suma = numero1 + numero2;
         int resto = numero1 - numero2;
         int multiplicacion = numero1 * numero2;
-        int division = numero1 / numero2;
-
-
-        JOptionPane.showMessageDialog(null, "El resultado de la suma es: " + suma+"\nLa resta es: "+ resto +"\nLa multiplicación es "+multiplicacion +"\nLa división es: "+ division, "Suma de dos enteros", JOptionPane.PLAIN_MESSAGE);
+        if (numero2 == 0) {
+            String division = "No se puede dividir para cero";
+        } else {
+            int division = numero1 / numero2;
+        }
+        JOptionPane.showMessageDialog(null, "El resultado de la suma es: " + suma + "\nLa resta es: " + resto + "\nLa multiplicación es " + multiplicacion + "\nLa división es: " + division, "Suma de dos enteros", JOptionPane.PLAIN_MESSAGE);*/
+    JFrame frame = new JFrame("Mi aplicación");
+    frame.setContentPane(new Form1().mainPanel);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.pack();
+    frame.setVisible(true);
     }
 }
