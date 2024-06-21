11f52e6aabdd3b8365f487397a98d279d4c9771b
Mateor13 <mateo.torres@epn.edu.ec>
2024-06-21 12:13:22 -0500

    Personalizar ventana

--- src/Java/Form1.java
+++ src/Java/Form1.java
@@ -6,24 +6,58 @@ import java.awt.event.ActionListener;
 
 public class Form1 {
 
-    private JButton btnOK;
+    private JButton rest;
     public JPanel mainPanel;
-    private javax.swing.JLabel JLabel;
-    private JButton borrarButton;
+    private JLabel LabelOK;
+    private JButton div;
+    private JTextField prim;
+    private JButton sum;
+    private JTextField seg;
+    private JLabel Resultado;
+    private JButton multi;
 
     public Form1() {
-        btnOK.addActionListener(new ActionListener() {
+        sum.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JLabel.setText("Hola a todos, yo soy Mateo Torres");
+                double num1 = Double.parseDouble(prim.getText());
+                double num2 = Double.parseDouble(seg.getText());
+                double suma = num1 + num2;
+                Resultado.setText(String.valueOf(suma));
             }
         });
-        borrarButton.addActionListener(new ActionListener() {
+        rest.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                JLabel.setText("");
+                double num1 = Double.parseDouble(prim.getText());
+                double num2 = Double.parseDouble(seg.getText());
+                double suma = num1 - num2;
+                Resultado.setText(String.valueOf(suma));
             }
         });
+        multi.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double num1 = Double.parseDouble(prim.getText());
+                double num2 = Double.parseDouble(seg.getText());
+                double mul = num1 * num2;
+                Resultado.setText(String.valueOf(mul));
+            }
+        });
+        div.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                double num1 = Double.parseDouble(prim.getText());
+                double num2 = Double.parseDouble(seg.getText());
+                double divi;
+                if(num2==0){
+                    Resultado.setText("No se puede dividir para cero");
+                }else{
+                    divi = num1/num2;
+                Resultado.setText(String.valueOf(divi));}
+            }
+        });
+
     }
 
 }
